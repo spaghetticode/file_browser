a5ea9a9fa3461cbf7b021aceca21a3047be1e721
andrea longhi <andrea@spaghetticode.it>
2013-05-02 21:22:53 +0200

    add Entry#ext + Entry types query methods

--- app/models/file_browser/entry.rb
+++ app/models/file_browser/entry.rb
@@ -4,11 +4,18 @@ module FileBrowser
     DIRECTORY = 'directory'
     TYPES     = [DIRECTORY, FILE]
 
-    attr_reader :name, :type
+    attr_reader :name, :type, :ext
 
     def initialize(name)
       @name = name
-      @type = find_type
+      @type = get_type
+      @ext  = get_ext
+    end
+
+    TYPES.each do |method_name|
+      define_method "#{method_name}?" do
+        self.class.const_get("#{method_name}".upcase) == type
+      end
     end
 
     def as_json(opts={})
@@ -17,8 +24,13 @@ module FileBrowser
 
     private
 
-    def find_type
+
+    def get_type
       TYPES.detect { |type| File.send("#{type}?", name) }
+    end
+
+    def get_ext
+      File.extname(name)[1..-1] if file?
     end
   end
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -10,11 +10,19 @@ module FileBrowser
 
     it { subject.should respond_to :name }
     it { subject.should respond_to :type }
+    it { subject.should respond_to :ext }
+    it { subject.should respond_to :file? }
+    it { subject.should respond_to :directory? }
+
     it { subject.type.should == Entry::DIRECTORY }
+    it { subject.should be_directory }
+    it { subject.should_not be_file }
 
     it 'has only file and directory types' do
       Entry::TYPES.should == [Entry::DIRECTORY, Entry::FILE]
     end
+
+
 
     it 'represents the name, path and type in json format' do
       json_hash = {:name => name, :type => Entry::DIRECTORY}.stringify_keys!
